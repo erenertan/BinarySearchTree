185b3247b4a2dfe4c9b7c67516b0e78b68bec3ba
erenertan <halileren1996@hotmail.com>
2016-12-21 16:41:12 +0300

    Almost done.

---
+++ src/Node.java
@@ -0,0 +1,14 @@
+/**
+ * Created by halil on 20.12.2016.
+ */
+public class Node {
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
